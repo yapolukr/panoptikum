2619ac45732a76de504bc751c6c18caea0715ec6
YanaFrolova1907 <106332725+YanaFrolova1907@users.noreply.github.com>
2022-06-27 12:31:17 +0200

    gjanfo-heroku secret key

--- panopt/settings.py
+++ panopt/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4*)6#oqng253uw1ujm^sy39xk7@x7!8vwz@d*-#(sos)hj(3%o'
+SECRET_KEY = os.environ.get("SECRET_KEY",'django-insecure-4*)6#oqng253uw1ujm^sy39xk7@x7!8vwz@d*-#(sos)hj(3%o')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
